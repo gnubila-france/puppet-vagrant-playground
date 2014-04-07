8d93480106d86d6c9f10fb5bc060f6ce33cc3d5a
Baptiste Grenier <bgrenier@gnubila.fr>
2014-04-07 19:23:38 +0200

    Import site.pp

---
+++ manifests/site.pp
@@ -0,0 +1,34 @@
+node 'puppet.local.lan' {
+  class { 'puppet::server':
+    servertype => 'standalone',
+    manifest   => '/etc/puppet/manifests/site.pp',
+    modulepath => ['$confdir/modules'],
+    ca         => true,
+  }
+  file { '/etc/puppet/hiera.yaml':
+    ensure  => 'present',
+    owner   => 'puppet',
+    group   => 'puppet',
+    mode    => '0640',
+    source  => 'puppet:///modules/site/puppet/hiera.yaml',
+    require => Package['puppetmaster'],
+    notify  => Service['puppetmaster'],
+  } ->
+  file { '/etc/hiera.yaml':
+    ensure  => 'link',
+    target  => '/etc/puppet/hiera.yaml',
+    require => File['/etc/puppet/hiera.yaml'],
+  }
+}
+
+node default {
+  hiera_include ('classes', [])
+
+  $packages = hiera_hash('packages', {})
+  create_resources('package', $packages)
+
+  $services = hiera_hash('services', {})
+  create_resources('service', $services)
+}
+
+# vim: set et sw=2 ts=2:
